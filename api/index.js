8f3d66ad877bd8e6c39ce35a3933578b55b1d93f
Nilo Neregato <neregato.nilo@gmail.com>
2020-06-03 00:55:41 +0200

    add proxy and import webcomponents

--- api/index.js
+++ api/index.js
@@ -1,15 +1,15 @@
 const axios = require('axios').default;
 
 module.exports = axios.create(
-  {
-    proxy: {
-      host: 'proxyseso.scania.com',
-      port: 8080,
-      protocol: '',
-      auth: {
-        username: process.env.PROXY_USER,
-        password: process.env.PROXY_PASSWORD
-      }
-    }
-  }
+  // {
+  //   proxy: {
+  //     host: 'proxyseso.scania.com',
+  //     port: 8080,
+  //     protocol: '',
+  //     auth: {
+  //       username: process.env.PROXY_USER,
+  //       password: process.env.PROXY_PASSWORD
+  //     }
+  //   }
+  // }
 );
--- server.js
+++ server.js
@@ -2,36 +2,28 @@ require('dotenv/config')
 const express = require('express');
 const server = express();
 const cors = require('cors');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const api = require('./api');
 
 server.use(cors());
 
 server.set('view engine', 'ejs');
 
-const getContents = (url) => new Promise((resolve, reject) => {
-  // requestWithProxy.get(url, (error, response, body) => {
-  //   if (error) return resolve("Error loading " + url + ": " + error.message);
+const createProxy = (path, target) =>
+  server.use(path, createProxyMiddleware({ 
+    target, 
+    changeOrigin: true, 
+    pathRewrite: {
+      [`^${path}`]: ''
+    } 
+  }
+));
 
-  //   return resolve(body);
-  // });
-  api.get(url)
-  .then(data => resolve(data))
-  .catch(error => resolve(`Error loading ${url}: ${error.response.data}`));
+createProxy('/header', process.env.HEADER_URL);
+createProxy('/plans', process.env.PLANS_URL);
+createProxy('/create-plan', process.env.CREATE_PLAN_URL);
 
-});
-
-server.get('/', (req, res) =>
-  Promise.all([
-    getContents(process.env.HEADER_URL),
-    getContents(process.env.PLANS_URL),
-    getContents(process.env.CREATE_PLAN_URL)
-  ]).then(responses =>
-    res.render('index', { header: responses[0], plans: responses[1], createPlan: responses[2] })
-  ).catch(error =>
-    res.send(error.message)
-  )
-);
-
+server.get('/', (req, res) => res.render('index'));
 
 const port = process.env.PORT || 8080;
 server.listen(port, () => {
